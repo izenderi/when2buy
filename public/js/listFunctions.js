18b4b079be101702dc2a0c22a302430ca535a2b9
Ziliang Zhang <ziz137@ucsd.edu>
2018-03-07 16:35:44 -0800

    bug fixed

--- public/js/listFunctions.js
+++ public/js/listFunctions.js
@@ -31,17 +31,12 @@ else
 }
 
 $( document ).ready(function() {
-  initializePage();
+
   // $("#newItemForm").submit(function(e) {
   //   e.preventDefault();
   //   alert("The item has been added");
   //   //app.get('/add', add.addItem);
   // });
-  $("#remove").click(function(e) {
-    console.log('click the remove');
-    e.preventDefault();
-
-  });
 
   $("#newLinkForm").submit(function(e) {
     e.preventDefault();
@@ -58,44 +53,8 @@ $( document ).ready(function() {
     alert("Copied the text: " + copyText.value);
   });
 
-  var list = document.querySelectorAll('.item');
-  for (var i=0; i<list.length; i++) {
-    list[i].addEventListener('click', function(ev) {
-<<<<<<< Updated upstream
-      if (ev.target.tagName === 'DIV') {
-        ev.target.classList.toggle('checked');
-        $(this).find("#remove").toggle();
-      }
-    }, false);
-  };
-=======
-    if (ev.target.tagName === 'DIV') {
-      ev.target.classList.toggle('checked');
-      $(this).find("#remove").toggle();
-    }
-  }, false);
-
-    // //click remove hide the item
-    // list[i].addEventListener('click', function(er) {
-    //   if(er.target.tagName === 'BUTTON'){
-    //     $(this).find(".item").toggle();
-    //   }
-    // }, false);
-    //   $("#itemList .item").click(function(e){
-    //     e.preventDefault();
-    //     $(this).remove();
-    //   });
-};
->>>>>>> Stashed changes
+  $("#itemList .item").click(function(e){
+    e.preventDefault();
+    $(this).remove();
+  });
 });
-
-function initializePage() {
-  console.log("hi");
-  $(".test").click(buttonUsed);
-}
-
-function buttonUsed(e) {
-  console.log("test");
-  e.preventDefault();
-  gtag('event', 'buttonUsed');
-}
