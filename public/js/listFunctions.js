a2017211ff6e74c49f6449fd78a1115eff75c4e5
Ziliang Zhang <ziz137@ucsd.edu>
2018-03-12 22:43:27 -0700

    zzl-branch merge

---
+++ public/js/listFunctions.js
@@ -0,0 +1,77 @@
+
+function hideItem() {
+$('.item').hide();
+}
+
+function toggleNewItemForm() {
+if($("#newItemDisplay").is(":visible")){$("#newItemDisplay").fadeOut();}
+else{$('#newItemDisplay').fadeIn();}
+
+}
+
+
+function toggleSettings(){
+if($("#remove").is(":visible")){$("#remove").fadeOut();}
+else{$("#remove").fadeIn();}
+}
+
+function toggleSuggestedItems(){
+alert("The milk has gone bad! please buy a new one before due day!");
+if($("#suggestedContainer").is(":visible"))
+  {
+    $("#suggestedContainer").fadeOut();
+    $(".suggestedItems").fadeOut();
+  }
+else
+  {
+    $("#suggestedContainer").fadeIn();
+    $(".suggestedItems").fadeIn();
+  }
+
+}
+
+$( document ).ready(function() {
+  initializePage();
+  // $("#newItemForm").submit(function(e) {
+  //   e.preventDefault();
+  //   alert("The item has been added");
+  //   //app.get('/add', add.addItem);
+  // });
+
+  $("#newLinkForm").submit(function(e) {
+    e.preventDefault();
+      /* Get the text field */
+    var copyText = document.getElementById("link");
+
+    /* Select the text field */
+    copyText.select();
+
+    /* Copy the text inside the text field */
+    document.execCommand("Copy");
+
+    /* Alert the copied text */
+    alert("Copied the text: " + copyText.value);
+  });
+
+  var list = document.querySelectorAll('.item');
+  for (var i=0; i<list.length; i++) {
+    list[i].addEventListener('click', function(ev) {
+      if (ev.target.tagName === 'DIV') {
+        ev.target.classList.toggle('checked');
+        $(this).find("#remove").toggle();
+      }
+    }, false);
+  };
+});
+
+function initializePage() {
+  // console.log("hi");
+  $(".test").click(buttonUsed);
+  $('.item').click(addItemDetails);
+}
+
+function buttonUsed(e) {
+  console.log("test");
+  e.preventDefault();
+  gtag('event', 'buttonUsed');
+}
